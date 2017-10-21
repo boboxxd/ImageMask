132d9c6be6ff54141e77cf1952d67e0c68d463f1
xudongxu <coona520@outlook.com>
2017-10-21 17:32:56 +0800

    主界面

---
+++ widget.cpp
@@ -0,0 +1,72 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<QPixmap>
+#include"getimage.h"
+#define LABELWIDTH 1080/2
+#define LABELHIGHT 720/2
+#include"xmltool.h"
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    initwindow();
+    connect(ui->rectbtn,SIGNAL(clicked(bool)),this,SLOT(drawRect()));
+    connect(ui->polygonbtn,SIGNAL(clicked(bool)),this,SLOT(drawPolygon()));
+    connect(ui->savebtn,SIGNAL(clicked(bool)),this,SLOT(save()));
+    connect(ui->connectbtn,SIGNAL(clicked(bool)),this,SLOT(onconnectbtn()));
+    connect(ui->clearbtn,SIGNAL(clicked(bool)),this,SLOT(onclearbtn()));
+}
+
+void Widget::onconnectbtn()
+{
+    qDebug()<<ui->lineEdit->text();
+    ui->label->clear();
+    //开启捕获图片的线程
+    Controller *con=new Controller;
+    connect(con,SIGNAL(image(QImage)),ui->label,SLOT(loadimage(QImage)));
+    con->operate(ui->lineEdit->text());
+}
+
+ void Widget::onclearbtn()
+ {
+     ui->label->clear();
+ }
+
+void Widget::initwindow()
+{
+    this->setWindowTitle("ImageMark");
+    this->setFixedSize(870,600);
+    ui->label->loadimage(":/background.png");
+    ui->lineEdit->setPlaceholderText("输入摄像机地址");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::drawRect()
+{
+    qDebug()<<"void Widget::drawRect()";
+    ui->label->setKind(0);
+    ui->label->update();
+}
+
+void Widget::drawPolygon()
+{
+    qDebug()<<"void Widget::drawPolygon()";
+    ui->label->setKind(1);
+    ui->label->update();
+}
+
+void Widget::save()
+{
+    if(ui->label->isEmpty()==false)
+    {
+        XmlTool  xml("text.xml",ui->label->getpoints());
+        qDebug()<<ui->label->getpoints();
+        qDebug()<<ui->label->getpoints().size()<<toString(ui->label->getpoints()[0]);
+    }
+
+}
---
+++ widget.h
@@ -0,0 +1,36 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include<QLabel>
+#include"common.h"
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    void initwindow();
+    ~Widget();
+
+signals:
+
+public slots:
+
+    void drawRect();//绘制矩形
+    void drawPolygon();//绘制多边形
+    void save();
+    void onconnectbtn();
+    void onclearbtn();
+
+private:
+    Ui::Widget *ui;
+
+};
+
+#endif // WIDGET_H
