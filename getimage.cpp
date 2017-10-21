6cec916091f664955c51ce148bedf3040cb93553
xudongxu <coona520@outlook.com>
2017-10-21 22:17:40 +0800

    取完图片后立即关闭 VideoCapture.release,解决读取大图片时，程序崩溃问题

--- getimage.cpp
+++ getimage.cpp
@@ -3,26 +3,32 @@
 
 void GetImage::doWork(const QString &path)
 {
-    qDebug()<<"doWork";
+    qDebug()<<"void GetImage::doWork(const QString &path)";
     QImage result;
     VideoCapture videoCap;
     Mat srcImage;
-    if(videoCap.open(path.toStdString().c_str()))
+    videoCap.open(path.toStdString().c_str());
+    if(videoCap.isOpened())
     {
         srcImage = Mat::zeros(videoCap.get(CV_CAP_PROP_FRAME_HEIGHT), videoCap.get(CV_CAP_PROP_FRAME_WIDTH), CV_8UC3);
-    }
         videoCap>>srcImage;
-        videoCap>>srcImage;
-
+        videoCap.release();
     if(srcImage.data)
     {
         cvtColor(srcImage, srcImage, CV_BGR2RGB);//Qt中支持的是RGB图像, OpenCV中支持的是BGR
         result=QImage((uchar*)(srcImage.data), srcImage.cols, srcImage.rows, QImage::Format_RGB888);
         emit resultReady(result);
     }
+    }else
+    {
+        qDebug()<<"连接失败";
+    }
+
 }
 
-void Controller::handleResults(const QImage _image)
+void Controller::handleResults(const QImage &_image)
 {
+    qDebug()<<"void Controller::handleResults(const QImage &_image)";
+    qDebug()<<_image.depth();
     emit image(_image);
 }
